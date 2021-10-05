4db6359f14af92d5ceb83a74f9e283fa3896400b
Raimy Mejia <raimym@goflowapp.com>
2021-10-05 10:40:10 -0400

    Identity Migrations

--- CarManagementApi/Migrations/20211005143913_Identity.Designer.cs
+++ CarManagementApi/Migrations/20211005143913_Identity.Designer.cs
@@ -4,14 +4,16 @@ using CarManagementApi.Models.Entities;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace CarManagementApi.Migrations
 {
     [DbContext(typeof(CarManagementContext))]
-    partial class CarManagementContextModelSnapshot : ModelSnapshot
+    [Migration("20211005143913_Identity")]
+    partial class Identity
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
