7378a679f05adc7a9e01d60982680fbda46bedd4
raimyy2m <raimymejia@gmail.com>
2021-10-07 08:31:10 -0400

    WIP

---
+++ CarManagementApi/Migrations/20211007025921_AdminUser.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CarManagementApi.Migrations
+{
+    public partial class AdminUser : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            //var id = Guid.NewGuid().ToString();
+
+            //migrationBuilder.InsertData(
+            //    table: "AspNetRoles",
+            //    columns: new[] { "Id", "Name", "NormalizedName", "ConcurrencyStamp" },
+            //    values: new object[]
+            //    {
+            //        ,
+            //        "Admin",
+            //        "ADMIN",
+            //        DateTime.Now.ToString()
+            //    }
+            //);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
